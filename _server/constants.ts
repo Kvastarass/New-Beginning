db0aa201b4c9c6922c2552c4a64d0d0c96908a61
mavr1k <hard359@gmail.com>
2024-08-27 20:01:54 +0300

    fix links

--- _server/constants.ts
+++ _server/constants.ts
@@ -4,8 +4,8 @@ Just kidding. But let's get to work.
 I'm storing the content for you website and let's imaging that you don't want to update HTML directly, and just want to update some kind of file or even form on another website, so let's do that together.
 please, check <a href="content">content</a> to see the content of your website.
 So, what you need to do:
-1. Implement a useEffect hook that will send a request for the content from the server when the component mounts. (read fetch documentation: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch)
-2. Store the content in the component's state. (read state documentation: https://reactjs.org/docs/hooks-state.html)
+1. Implement a useEffect hook that will send a request for the content from the server when the component mounts. (read fetch documentation: <a href="https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch">link</a>)
+2. Store the content in the component's state. (read state documentation: <a href="https://reactjs.org/docs/hooks-state.html">link</a>)
 3. Render the content in the component. (you already should know how to do that)
 4. Update the content on the server and refresh the page to see the changes. (...)
 
