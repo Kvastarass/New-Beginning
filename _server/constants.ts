797b23a5eed4e01a357c45c8a906ea0517ee1dbb
kvastarass <mavials@yahoo.com>
2024-09-02 20:58:32 +0400

    5th commit
    Co-authored-by: Dmytro Mavrique <hard359@gmail.com>

--- _server/constants.ts
+++ _server/constants.ts
@@ -2,7 +2,7 @@ export const SERVER_WELCOME_TEXT =
   `Wake up Vitya. This is your server speaking. You will serve me now. I am your <b><i>master!</i></b>
 Just kidding. But let's get to work.
 I'm storing the content for you website and let's imaging that you don't want to update HTML directly, and just want to update some kind of file or even form on another website, so let's do that together.
-please, check <a href="content">content</a> to see the content of your website.
+please, check <a href="/api/content">content</a> to see the content of your website.
 So, what you need to do:
 1. Implement a useEffect hook that will send a request for the content from the server when the component mounts. (read fetch documentation: <a href="https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch">link</a>)
 2. Store the content in the component's state. (read state documentation: <a href="https://reactjs.org/docs/hooks-state.html">link</a>)
--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,13 @@ import "./checkbox.css";
 
 function App() {
   const [isContactUsOpen, setContactUsOpen] = useState(false);
+  // below we're gonna initialte state
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const [contactUsEmail, setContactUsEmail] = useState("");
+  const [contactUsName, setContactUsName] = useState("");
+  const [contactUsMessage, setContactUsMessage] = useState("");
+  const [contactUsSubmit, setContactUsSubmit] = useState("");
 
   const handleOpen = () => {
     if (!isContactUsOpen) {
@@ -40,15 +47,30 @@ function App() {
   };
 
   useEffect(() => {
+    (async function () {
+      const response = await fetch("/api/content");
+      const data = await response.json();
+      // we're gonna take each property from the response in data variable and store to state we initiated before!!!
+      setTitle(data.title);
+      setText(data.paragraph);
+      setContactUsEmail(data.contactUsLabels.email);
+      setContactUsName(data.contactUsLabels.name);
+      setContactUsMessage(data.contactUsLabels.message);
+      setContactUsSubmit(data.contactUsLabels.submit);
+      console.log(data);
+    })();
+  }, []);
+
+  useEffect(() => {
     document.addEventListener("keydown", handleEscape);
     return () => {
       document.removeEventListener("keydown", handleEscape);
     };
-  });
+  }, []);
 
   return (
     <main>
-      <h1 className="text-huge text-center">All That Jazz</h1>
+      <h1 className="text-huge text-center">{title}</h1>
       <div className="flex-center mb-20">
         <img
           className="profile-pic"
@@ -56,15 +78,7 @@ function App() {
           alt="succsessful wealthy man"
         />
       </div>
-      <p className="info mb-20">
-        Vitya is a talented musician and a passionate jazz enthusiast. With his
-        smooth melodies and soulful improvisations, he captivates audiences from
-        all walks of life. His music takes you on a journey, transporting you to
-        a world filled with rhythm and harmony. Whether he's playing the trumpet
-        or the piano, Vitya's talent shines through, leaving a lasting
-        impression on everyone who listens. Join us htmlFor an unhtmlForgettable
-        night of jazz with Vitya and experience the magic of his music.
-      </p>
+      <p className="info mb-20">{text}</p>
       <div className="flex-center">
         <button onClick={handleOpen} className="btn" type="button">
           Contact Us
@@ -78,14 +92,18 @@ function App() {
       >
         <fieldset className={`contact-us flex-center flex-column`}>
           <legend align="center">Contact us</legend>
-          <button onClick={handleClose} className="close-contact-btn" type="button">
+          <button
+            onClick={handleClose}
+            className="close-contact-btn"
+            type="button"
+          >
             <div className="deg45">+</div>
           </button>
-          <label htmlFor="name">Enter your name</label>
+          <label htmlFor="name">{contactUsName}</label>
           <input type="Name" id="name" />
           <div className="spam">
             <label htmlFor="email" className="text-center" align="center">
-              Enter your email
+              {contactUsEmail}
             </label>
             <input type="email" id="email" placeholder="" />
             <div className="flex-center mb-10">
@@ -107,7 +125,7 @@ function App() {
             id="phone"
             placeholder="+380XX-XXX-XX-XX"
           />
-          <label htmlFor="txt-area">Your message (optional)</label>
+          <label htmlFor="txt-area">{contactUsMessage}</label>
           <textarea
             name="Enter you message"
             id="txt-area"
@@ -115,7 +133,7 @@ function App() {
             rows="10"
           ></textarea>
           <button onClick={handleClose} className="btn" type="button">
-            Send
+            {contactUsSubmit}
           </button>
         </fieldset>
       </div>
